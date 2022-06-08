43d4e48909d2b6056ce0eeede81c06acfe2c814b
jeetsimrann <61161783+jeetsimrann@users.noreply.github.com>
2022-06-08 15:32:45 -0400

    Frist Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $(".menu-toggle").click(function () {
+      $(".menu-toggle").toggleClass("menu-open");
+      $(".menu").toggleClass("active");
+    });
+    $(".link").click(function () {
+      $(".menu-toggle").toggleClass("menu-open");
+      $(".menu").toggleClass("active");
+    });
+    $(".videoOne").click(function () {
+      $(".overlayOne").toggleClass("active");
+    });
+    $(".videoTwo").click(function () {
+      console.log("i was clicked");
+      $(".overlayTwo").toggleClass("active");
+    });
+    $(".videoThree").click(function () {
+      $(".overlayThree").toggleClass("active");
+    });
+    $(".videoFour").click(function () {
+      $(".overlayFour").toggleClass("active");
+    });
+    $(".closeOverlayOne").click(function () {
+      $(".overlayOne").toggleClass("active");
+    });
+    $(".closeOverlayTwo").click(function () {
+      console.log("i was clicked");
+      $(".overlayTwo").toggleClass("active");
+    });
+    $(".closeOverlayThree").click(function () {
+      $(".overlayThree").toggleClass("active");
+    });
+    $(".closeOverlayFour").click(function () {
+      $(".overlayFour").toggleClass("active");
+    });
+  });
+  
